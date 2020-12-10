57a2eabdc27d365611c111abf3386c3c063b3de8
leohc87 <38429138+leohc87@users.noreply.github.com>
2020-12-10 19:01:09 -0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,17 +1,19 @@
 ﻿using System;
 
-namespace DigitalInovvationOne
+namespace EstruturaDoPrograma
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            int numeroDeVezes = 5;
-
-            for(int i=0; i < numeroDeVezes; i++)
-            {
-                Console.WriteLine($"Bem-vindo ao curso de .NET {i}");
+            var s = new Pilha();
+            s.Empilha(1);
+            s.Empilha(10);
+            s.Empilha(100);
+            Console.WriteLine(s.Desempilha());
+            Console.WriteLine(s.Desempilha());
+            Console.WriteLine(s.Desempilha());
             }
         }
     }
-}
+
